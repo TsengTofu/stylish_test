c14ec4f8d05460ef79bd846e01a9eafe98ac41eb
Tofu <tsengbatty@gmail.com>
2019-03-05 16:02:37 +0800

    hello

--- js/FB_api.js
+++ js/FB_api.js
@@ -1,54 +1,57 @@
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+        localStorage.setItem('accessToken', response.authResponse.accessToken);
+        testAPI();
+    } else {
+        // The person is not logged into your app or we are unable to tell.
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';
+    }
+}
 
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: "306113546764828", // 這是自己的app ID
-        cookie: true, // enable cookies allow the server to access
-        xfbml: true, // parse social plugins on this page
-        version: "v3.2" // The Graph API version to use for the call
-    });
-    FB.AppEvents.logPageView();
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
     FB.getLoginStatus(function (response) {
         statusChangeCallback(response);
-    });
-};
-
-// 載入facebook javascript sdk
-(function (d, s, id) {
-    var js,
-        fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
-function checkLoginState() {
-    FB.login(function (response) {
-        statusChangeCallback(response);
-        // var accessToken = response.authResponse.accessToken;
-        localStorage.setItem('accessToken', response.authResponse.accessToken);
     }, {
             scope: 'email,user_photos',
             return_scopes: true
         });
 }
 
-function statusChangeCallback(response) {
-    console.log("statusChangeCallback");
-    console.log(response);
-    // response object 會回傳目前的 status，讓 app 知道目前使用者的 login status
-    if (response.status === "connected") {
-        testAPI();
-        localStorage.setItem('accessToken', response.authResponse.accessToken);
-    } else {
-        // 目前使用者並沒有登入或是我們不知道發生了甚麼事
-        //   document.getElementById("status").innerHTML =
-    }
-}
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '306113546764828',
+        cookie: true,  // enable cookies to allow the server to access 
+        // the session
+        xfbml: true,  // parse social plugins on this page
+        version: 'v3.2' // The Graph API version to use for the call
+    });
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
 
+};
 
+// Load the SDK asynchronously
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
-
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
     // console.log("Welcome!  Fetching your information.... ");
     FB.api('/me?fields=id,name,email,picture', function (response) {
@@ -82,7 +85,15 @@ function testAPI() {
         const fbBtn = document.querySelector(".fb");
         fbBtn.style.display = "none";
     });
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
