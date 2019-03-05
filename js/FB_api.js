27522a55fdf26eb463c6d9ba2b9de97de4fa166d
Tofu <tsengbatty@gmail.com>
2019-03-05 17:44:04 +0800

    test

--- js/FB_api.js
+++ js/FB_api.js
@@ -8,6 +8,7 @@ function statusChangeCallback(response) {
     if (response.status === 'connected') {
         localStorage.setItem('accessToken', response.authResponse.accessToken);
         console.log(localStorage.setItem('accessToken', response.authResponse.accessToken))
+        console.log(accessToken);
         testAPI();
     } else {
         // The person is not logged into your app or we are unable to tell.
