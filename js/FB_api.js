0a06bc9f70e00c02c8154068eb07d045529a74eb
Tofu <tsengbatty@gmail.com>
2019-03-06 09:31:14 +0800

    test_2

--- js/FB_api.js
+++ js/FB_api.js
@@ -2,7 +2,9 @@ function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
     if (response.status === 'connected') {
-        localStorage.setItem('accessToken', response.authResponse.accessToken);
+        
+        localStorage.setItem('accessToken', JSON.stringify(response.authResponse.accessToken));
+        console.log(localStorage.setItem('accessToken', JSON.stringify(response.authResponse.accessToken)))
         let currentUrl = window.location.href;
         console.log(currentUrl);
         if (currentUrl.includes('profile')) {
