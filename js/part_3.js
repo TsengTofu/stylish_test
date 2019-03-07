81009e115f37a78f335dbd1742bc47a4b11642e8
Tofu <tsengbatty@gmail.com>
2019-03-07 17:06:10 +0800

    do

--- js/part_3.js
+++ js/part_3.js
@@ -117,26 +117,7 @@ const printData = data => {
   });
 };
 
-const categoryMenus = document.querySelector(".menu");
-const categoryMenuList = document.querySelectorAll(".menu li");
-const changeBtn = btn => {
-  //只有切換按鈕的時候才需要刪除
-  while (productList.firstChild) {
-    productList.removeChild(productList.firstChild);
-  }
-  if (btn.target.textContent === "女裝") {
-    getDataParse("products/women");
-    productCategory = "products/women";
-  } else if (btn.target.textContent === "男裝") {
-    getDataParse("products/men");
-    productCategory = "products/men";
-  } else {
-    getDataParse("products/accessories");
-    productCategory = "products/accessories";
-  }
-};
 
-categoryMenus.addEventListener("click", changeBtn);
 
 
 // 以上是part3
